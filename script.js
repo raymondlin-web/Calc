f954c8830653188cc0a8144c6aa59e4303326d68
raymondlin-web <ryl262@nyu.edu>
2020-04-07 12:50:19 -0400

    Java

---
+++ script.js
@@ -0,0 +1,17 @@
+var number = number;
+function sqrethis(number) {
+  console.log(number*number);
+}
+var name = fortname;
+var fortune = name.charAt(0);
+function future(name) {
+  if (fortune < "G");
+    console.log("Good luck with life");
+    else if (fortune < "N");
+      console.log("Bad luck incoming");
+    else if (fortune < "T");
+      console.log("Wow great luck for you");
+    else {
+      console.log("Pretty OK for now");
+    }
+}
